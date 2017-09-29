b76db5d1682c8ef9611c86705e54dda3e0561943
MOLHAMKAADAN\Molham <molham_kaadan@hotmail.com>
2017-09-29 16:05:47 -0600

    Milestone 1 Demo

---
+++ Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpHeight;
+    public float groundCheckRadius;
+    public Transform groundCheck;
+    public LayerMask maskGround;
+    private bool grounded;
+    private Animator anime;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        anime = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, maskGround);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            anime.SetBool("Jump", true);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            //anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+
+        anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
+
+
+
+    }
+
+     void OnCollisionEnter2D(Collision2D coll)
+    {
+        anime.SetBool("Jump", false);
+    }
+}
