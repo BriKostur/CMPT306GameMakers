626983494379ad0d30cf25fc5d6154388b27daef
Spencer Brown <slb097@mail.usask.ca>
2017-10-08 15:31:01 -0600

    The following modifications were made to project:
    PlayerMovement.cs: - all lines pertaining to ground checking were removed
                                         - added flag to check if a jump had occurred to prevent infinite jumping
                                         - modified startup to have a spawn point for the character to appear
                                         - general formatting adjustment
    
    Template.unity: - introduced a death boundary, where if the character falls below the level, he will respawn (currently set to the initial spawn point)
                                - added empty spawn point object for character to spawn at
    
    DeathWarp.cs (NEW) : - a script built to handle what to do if the character hits the death boundary in Template.unity
    
    TODO:
    - add new ground checking parameter to PlayerMovement.cs
             - doing this to prevent being able to jump when falling
             - this will be tied to checking a tag, which will be placed on an extremely thin invisible object attached to the top surface of every platform
             - entire platform can not have tag, otherwise jumping up and bumping a platform with character's head would reset flag, allowing jump in mid-air
    - create object to tie onto platforms (see above), save as prefab
    - adjust physics to not feel so "floaty"
    - minor animation adjustments
    - basically everything else
    
    ~ Spencer

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -7,47 +7,45 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;
     public float jumpHeight;
-    //public float groundCheckRadius;
-    //public Transform groundCheck;
-    //public LayerMask maskGround;
-    //private bool grounded;
     private Animator anime;
+	private bool inAir; // Flag to prevent the player from jumping infinitely
+	public Transform warpDestination;
 
 
     // Use this for initialization
     void Start()
     {
+		inAir = false;
+		gameObject.transform.position = warpDestination.position; // Set spawn point
         anime = GetComponent<Animator>();
     }
  
     // Update is called once per frame
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
-            anime.SetBool("Jump", true);
-        }
+		if (Input.GetKey (KeyCode.Space))
+		{
+			if (inAir == false) // If we are not already jumping
+			{
+				inAir = true; // We are now jumping
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+				anime.SetBool ("Jump", true);
+			}
+		}
         if (Input.GetKey(KeyCode.RightArrow))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            //anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
-
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
-
         anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
-
-
-
     }
 
      void OnCollisionEnter2D(Collision2D coll)
     {
+		inAir = false; // We are no longer jumping
         anime.SetBool("Jump", false);
     }
 }
