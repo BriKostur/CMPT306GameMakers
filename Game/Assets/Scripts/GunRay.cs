c1312ceee1477dec8f20e034d6c1b7fee501b056
Brianne Kostur <bdk511@mail.usask.ca>
2017-10-13 13:47:57 -0600

    Cleaned up and commented GunRay script

--- Game/Assets/Scripts/GunRay.cs
+++ Game/Assets/Scripts/GunRay.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GunRay : MonoBehaviour {
+
+	//gunRay transform
+	Transform myTransform;
 	//item the gun holds
 	GameObject item;
-	Transform myTransform;
+	//the character
 	GameObject oldMan;
 	//timer for how long to show gunray when shot
 	int timer;
 	//if gun ray is visible
 	bool rayVisible;
+	//gunRay renderer
 	LineRenderer myLineRend;
+	//gunRay collider
 	CapsuleCollider2D myColl;
 	//show ray
 	Vector3 visiblePos;
@@ -19,65 +24,86 @@ public class GunRay : MonoBehaviour {
 	Vector3 hidePos;
 
 
-
-	// Use this for initialization
 	void Start () {
+		//initialize
+		myTransform = GetComponent<Transform>();
 		item = null;
 		oldMan = GameObject.Find ("OldMan");
-		myTransform = GetComponent<Transform>();
-		visiblePos = new Vector3(0f, -0.03f, 0f);
-		hidePos = new Vector3 (100f, 100f, -10f);
-		myTransform.localPosition = hidePos;
 		timer = 0;
 		rayVisible = false;
 		myLineRend = GetComponent<LineRenderer>();
 		myColl = GetComponent<CapsuleCollider2D>();
+		visiblePos = new Vector3(0f, -0.03f, 0f);
+		hidePos = new Vector3 (100f, 100f, -10f);
 
-
-
+		//set gunRay to hide position
+		myTransform.localPosition = hidePos;
 	}
 
-	// Update is called once per frame
+
 	void Update () {
+		//if fire key is pressed shoot or drop item
 		if (Input.GetKeyDown (KeyCode.F)) {
+			//get direction of character to set ray direction
 			float manDir= oldMan.GetComponent<Rigidbody2D>().velocity.x;
-			//if no item display gunray
+
+			//if hold no item, display gunray
 			if (item == null) {
-				//fire gun ray
+				//set gunray position
 				myTransform.localPosition = visiblePos;
+			
+				//set direction of ray and collider
 				Vector3 lineEndPoint = myLineRend.GetPosition(1);
 
-				//set direction of ray and collider
+				//if man facing right or forward
 				if (manDir >= 0) {
+					//set ray direction and collider to the right
 					myLineRend.SetPosition (1, new Vector3 (Mathf.Abs(lineEndPoint.x), lineEndPoint.y, lineEndPoint.z));
 					myColl.offset = new Vector2(Mathf.Abs(myColl.offset.x),myColl.offset.y);
-				} else {
+				} 
+
+				//if man facing left
+				else {
 					myLineRend.SetPosition (1, new Vector3 (-(Mathf.Abs(lineEndPoint.x)), lineEndPoint.y, lineEndPoint.z));
 					myColl.offset = new Vector2(-(Mathf.Abs(myColl.offset.x)),myColl.offset.y);
 				}
+
+				//set rayVisible true and reset timer
 				rayVisible = true;
 				timer = 0;
+			
 			//else place object beside old man
 			} else {
+				//if man facing right or forward place object to right of man
 				if (manDir >= 0) {
 					item.transform.position = new Vector3 (oldMan.transform.position.x + 1.5f, oldMan.transform.position.y, oldMan.transform.position.z);
-				} else {
+				} 
+
+				//if man facing left place object to left of man
+				else {
 					item.transform.position = new Vector3 (oldMan.transform.position.x + -1.5f, oldMan.transform.position.y, oldMan.transform.position.z);
 				}
 				item = null;
 			}
-		//when timer has hit 15 stop gun ray
-		} else if(timer > 15 && rayVisible){
+		} 
+
+		//when timer has hit 15 hide gun ray if its visible
+		else if(timer > 15 && rayVisible){
 			myTransform.localPosition = hidePos;
 			timer = 0;
 		}
+
+		//increment timer
 		timer = timer + 1;
 	}
 
+
 	void OnCollisionEnter2D(Collision2D coll)
 	{
+		//get object collided with
 		GameObject collObj = coll.collider.gameObject; 
-		//if collides with MMGBox set and hide item
+
+		//if collided with MMGBox set and hide item
 		if (collObj.tag == "MMGBox"){
 			item = collObj;
 			collObj.transform.localPosition = new Vector3 (-100f, -100f, -20f);
