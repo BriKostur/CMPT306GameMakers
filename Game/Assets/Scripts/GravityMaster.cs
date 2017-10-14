abd18c606600a2a1e7e6af01e620c9a2d7f55dee
SKY-DESKTOP\Skyler <skylersampson@shaw.ca>
2017-10-13 19:52:29 -0600

    Added first implentation fo gravity boots, player controls do not account for altered gravity yet, but the player itself rotates correctly. Scene modified for testing of boots. Also added simple gravity modifcation object, controlled form unbity via public variables.

---
+++ Game/Assets/Scripts/GravityMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityMaster : MonoBehaviour {
+	public Vector2 grav;
+	public bool doUpdate;
+
+	void Start () {
+		grav = Physics.gravity;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (doUpdate){
+			Physics2D.gravity = grav;
+			doUpdate = false;
+		}
+	}
+}
--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -4,35 +4,71 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	Vector2 moveVec;
+	Vector2 curVec;
+	Rigidbody2D phys;
 
+	public float maxVel = 16;
+	public float minVel = 4;
     public float moveSpeed;
     public float jumpHeight;
-    public float groundCheckRadius;
-    public Transform groundCheck;
-    public LayerMask maskGround;
-    private bool grounded;
+  //  public float groundCheckRadius;
+   // public Transform groundCheck;
+ //   public LayerMask maskGround;
+  //  private bool grounded;
     private Animator anime;
-
+	private bool right;
 
     // Use this for initialization
     void Start()
     {
+		phys = GetComponent<Rigidbody2D> ();
         anime = GetComponent<Animator>();
+		moveVec.x = 0;
+		moveVec.y = 0;
     }
+
     private void FixedUpdate()
     {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, maskGround);
+		if (!GetComponentInChildren<MagBoots> ().isActive) {
+			transform.up = -Physics2D.gravity;
+		}
+  //      grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, maskGround);
     }
+		
     // Update is called once per frame
     void Update()
     {
+		
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
             anime.SetBool("Jump", true);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+
+		//Alternate movement code
+		curVec = phys.velocity;
+		moveVec.x = moveSpeed * Input.GetAxis ("Horizontal");
+
+		if (moveVec.x < minVel && moveVec.x > -minVel && moveVec.x!=0) {
+			if (moveVec.x > 0){ 
+				moveVec.x = minVel;
+			}else{
+				moveVec.x = -minVel;
+			}
+		}
+
+		if ((curVec.x + moveVec.x) > maxVel) {
+			moveVec.x = maxVel- curVec.x;
+		}
+		if (curVec.x + moveVec.x < -maxVel) {
+			moveVec.x = -maxVel - curVec.x;
+		}
+
+		GetComponent<Rigidbody2D>().AddForce(moveVec); //*/
+
+        /* if (Input.GetKey(KeyCode.RightArrow))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
             //anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
@@ -41,7 +77,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
+        }//*/
 
         anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
 
