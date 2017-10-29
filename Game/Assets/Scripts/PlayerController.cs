7b1b95bf46101655e5ad06d78d30edf619a3d7df
SKY-DESKTOP\Skyler <skylersampson@shaw.ca>
2017-10-29 15:57:41 -0600

    Improved player movement with gravity boots, now has porper min/max veloictys, jumping and animations still not handled, renamed testing level to GravTestZone

--- Game/Assets/Scripts/PlayerController.cs
+++ Game/Assets/Scripts/PlayerController.cs
@@ -12,10 +12,6 @@ public class PlayerController : MonoBehaviour
 	public float minVel = 4;
     public float moveSpeed;
     public float jumpHeight;
-  //  public float groundCheckRadius;
-   // public Transform groundCheck;
- //   public LayerMask maskGround;
-  //  private bool grounded;
     private Animator anime;
 	private bool right;
 
@@ -33,55 +29,32 @@ public class PlayerController : MonoBehaviour
 		if (!GetComponentInChildren<MagBoots> ().isActive) {
 			transform.up = -Physics2D.gravity;
 		}
-  //      grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, maskGround);
     }
 		
     // Update is called once per frame
     void Update()
     {
-		
-
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
             anime.SetBool("Jump", true);
         }
 
-		//Alternate movement code
+
 		curVec = phys.velocity;
 		moveVec.x = moveSpeed * Input.GetAxis ("Horizontal") * transform.right.x;
 		moveVec.y = moveSpeed * Input.GetAxis ("Horizontal") * transform.right.y;
-		/*if (moveVec.x < minVel && moveVec.x > -minVel && moveVec.x!=0) {
-			if (moveVec.x > 0){ 
-				moveVec.x = minVel;
-			}else{
-				moveVec.x = -minVel;
-			}
+		if (moveVec.magnitude < minVel) {
+			moveVec.Normalize ();
+			moveVec = moveVec * minVel;
 		}
-
-		if ((curVec.x + moveVec.x) > maxVel) {
-			moveVec.x = maxVel- curVec.x;
+		if ((curVec.magnitude + moveVec.magnitude) > maxVel) {
+			moveVec.Normalize ();
+			moveVec = moveVec * (maxVel - curVec.magnitude);
 		}
-		if (curVec.x + moveVec.x < -maxVel) {
-			moveVec.x = -maxVel - curVec.x;
-		}*/
-		GetComponent<Rigidbody2D>().AddForce(moveVec); //*/
-
-        /* if (Input.GetKey(KeyCode.RightArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            //anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
-
-        }
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }//*/
+		GetComponent<Rigidbody2D>().AddForce(moveVec);
 
         anime.SetFloat("Speed", GetComponent<Rigidbody2D>().velocity.x);
-
-
-
     }
 
      void OnCollisionEnter2D(Collision2D coll)
