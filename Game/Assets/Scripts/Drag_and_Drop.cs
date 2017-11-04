fde351c9f92f120e3ea06cb15195fffcf029559c
Johnathan Tam <John@uofs-10-228-2-136.usask.ca>
2017-11-04 17:41:40 -0600

    polished drag and drop and play function

---
+++ Game/Assets/Scripts/Drag_and_Drop.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag_and_Drop : MonoBehaviour {
+	
+	private Vector3 offset;
+	private Vector3 mousePosition;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown() {
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f));
+	}
+		
+	void OnMouseDrag() {
+		/**
+		if (gameObject.tag != "Prefab") {
+			mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.0f);
+			transform.position = Camera.main.ScreenToWorldPoint (mousePosition) + offset;
+		}
+		**/
+		mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 5.0f);
+		transform.position = Camera.main.ScreenToWorldPoint (mousePosition) + offset;
+	}
+
+	public void Spawn() {
+		Instantiate (this, new Vector2(-10, 3), Quaternion.identity);
+	}
+}
---
+++ Game/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour {
+
+	Rigidbody2D r_body;
+	public Canvas levelEdit;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	public void Play() {
+		GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+		foreach (GameObject go in allObjects) {
+			go.SendMessage("disableScript", SendMessageOptions.DontRequireReceiver);
+		}
+
+		GameObject player = GameObject.FindGameObjectWithTag ("Player");
+		r_body = player.GetComponent<Rigidbody2D> ();
+		r_body.constraints = RigidbodyConstraints2D.None;
+		r_body.constraints = RigidbodyConstraints2D.FreezeRotation;
+		r_body.gravityScale = 1;
+		levelEdit.GetComponent<Canvas> ().enabled = false;
+	}
+}
