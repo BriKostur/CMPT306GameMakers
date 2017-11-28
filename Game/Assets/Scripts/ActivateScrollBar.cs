48104f41182000ca910c98c8ae7299555785fa72
MOLHAMKAADAN\Molham <molham_kaadan@hotmail.com>
2017-11-28 10:44:01 -0600

    Completed list of buttons for made levels

---
+++ Game/Assets/Scripts/ActivateScrollBar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateScrollBar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject scroll = transform.Find("ScrollBar Vertical").gameObject;
+        scroll.SetActive(true);
+    }
+}
