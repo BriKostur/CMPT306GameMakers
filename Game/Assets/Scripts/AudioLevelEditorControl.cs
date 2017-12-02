53f5a81fd99e03a402460f7ffcaca56716133641
Spencer Brown <slb097@mail.usask.ca>
2017-12-02 00:17:15 -0600

    Made modification to audio controller for level editor. Music now fades-out on level exit.

--- Game/Assets/Scripts/AudioLevelEditorControl.cs
+++ Game/Assets/Scripts/AudioLevelEditorControl.cs
@@ -13,9 +13,20 @@ public class AudioLevelEditorControl : MonoBehaviour {
 	float slowDownFactor = 1.21587f; // Speed factor adjustment to go fast -> slow
 	float speedUpFactor = 0.82246f; // Speed factor adjustment to go slow -> fast
 
+	float fadeRate = 3.0f;
+	float interpolationValue = 0.0f;
+	float fadedVolume;
+	bool fadeAudioOut = false;
+
 	// Use this for initialization
 	void Start () {
 		slow.Play (); // Play slow track on Level Editor entry
+	}
+
+	void Update() {
+		if (fadeAudioOut == true) {
+			audioFade ();
+		}
 	}
 
 	void FixedUpdate() {
@@ -26,6 +37,10 @@ public class AudioLevelEditorControl : MonoBehaviour {
 		if (speedForLoop == true && fast.isPlaying == false) {
 			fast.time = 53.834f; // Loop position
 			fast.Play ();
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			fadeAudioOut = true;
+			Debug.Log (fadeAudioOut);
 		}
 	}
 
@@ -47,5 +62,31 @@ public class AudioLevelEditorControl : MonoBehaviour {
 		fast.Stop ();
 		slow.time = playTime;
 		slow.Play ();
+	}
+
+	void audioFade() {
+		interpolationValue += Time.deltaTime * fadeRate;
+		fadedVolume = Mathf.Lerp (1f, 0f, interpolationValue);
+		slow.volume = fadedVolume;
+
+		// Modify interpolation values to emulate a more realistic fade-out
+		if (slow.volume >= 0.4f && slow.volume < 0.5f) {
+			fadeRate = 1.0f;
+		}
+		if (slow.volume >= 0.3f && slow.volume < 0.4f) {
+			fadeRate = 0.8f;
+		}
+		if (slow.volume >= 0.2f && slow.volume < 0.3f) {
+			fadeRate = 0.6f;
+		}
+		if (slow.volume >= 0.1f && slow.volume < 0.2f) {
+			fadeRate = 0.4f;
+		}
+		if (slow.volume >= 0.03f && slow.volume < 0.1f) {
+			fadeRate = 0.1f;
+		}
+		if (slow.volume < 0.03f) {
+			fadeRate = 0.05f;
+		}
 	}
 }
