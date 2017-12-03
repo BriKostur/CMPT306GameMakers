526fdfb65dff671d7a1a3e5b941f240ca83e8462
Spencer Brown <slb097@mail.usask.ca>
2017-12-03 13:11:38 -0600

    All functioning now. Made small modification to fade canvas in Level Editor to fix the name entry field when saving a level (was originally covering it up)

--- Game/Assets/Scripts/LevelExiter.cs
+++ Game/Assets/Scripts/LevelExiter.cs
@@ -7,6 +7,7 @@ public class LevelExiter : MonoBehaviour {
 
 	public FadeTransition _fadeTransition;
 	public Canvas levelEdit;
+	public Canvas fade;
 
 	void Update() {
 		if(Input.GetKeyDown(KeyCode.Escape)) {
@@ -19,18 +20,15 @@ public class LevelExiter : MonoBehaviour {
 	}
 
 	IEnumerator executeSwitch() {
+		if (fade.enabled == false) {
+			fade.enabled = true;
+		}
 		Scene currentScene = SceneManager.GetActiveScene ();
 		_fadeTransition.initiate (false);
-		if (currentScene.name == "Level 1" || currentScene.name == "Level 2" || currentScene.name == "Level 3" || 
-			currentScene.name == "Level 4" || currentScene.name == "Level 5" || currentScene.name == "Level 6") {
-			yield return new WaitForSeconds (2);
-			SceneManager.LoadScene ("Pre-Created Levels"); // Name of button must match scene name, otherwise error
-		}
-		else
-		{
+		if (currentScene.name == "Level Editor") {
 			levelEdit.GetComponent<Canvas> ().enabled = false;
-			yield return new WaitForSeconds (2);
-			SceneManager.LoadScene ("Title Screen"); // Name of button must match scene name, otherwise error
 		}
+		yield return new WaitForSeconds (2);
+		SceneManager.LoadScene ("Title Screen"); // Name of button must match scene name, otherwise error
 	}
 }
